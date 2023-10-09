52dd730881be13b1a91c8f61c59def4e25f99838
Moudou <dplayerd@gmail.com>
2023-10-09 11:51:58 +0800

    【開發】讀取一般欄位

---
+++ TryJson/Models/ResultObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryJson.Models
+{
+    class ResultObject
+    {
+        public string Path { get; set; }
+        public string ColumnName { get; set; }
+        public string OrgValue { get; set; }
+        public string NewValue { get; set; }
+
+    }
+}
